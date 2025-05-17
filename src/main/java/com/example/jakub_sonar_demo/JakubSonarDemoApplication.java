e6c8f7ddcf0f241f3abd6dc3674fb146c95bcd66
OpalinskiJakub <jakopal1125@gmail.com>
2025-05-17 10:45:21 +0200

    init

---
+++ src/main/java/com/example/jakub_sonar_demo/JakubSonarDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.jakub_sonar_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JakubSonarDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JakubSonarDemoApplication.class, args);
+	}
+
+}
